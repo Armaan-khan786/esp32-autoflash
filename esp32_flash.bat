fe2095d0c60fdae952c20ccada2e0c6761345587
Armaan-khan786 <a75777129@gmail.com>
2026-02-03 13:32:17 +0530

    self-hosted-pc-runner

--- esp32_flash.bat
+++ esp32_flash.bat
@@ -5,7 +5,7 @@ echo ===============================
 
 set PORT=COM6
 set FQBN=esp32:esp32:esp32
-set SKETCH=loopback
+set SKETCH=uart_loopback
 
 arduino-cli.exe compile --fqbn %FQBN% %SKETCH%
 IF ERRORLEVEL 1 (
